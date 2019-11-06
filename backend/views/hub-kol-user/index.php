45206b9cce596dbbe136d2aff3d0dfeb0f59a8f3
Clown <rd@promer.co>
2019-11-06 19:18:08 +0800

    moidfy

--- backend/views/hub-kol-user/index.php
+++ backend/views/hub-kol-user/index.php
@@ -1,7 +1,7 @@
 
 <?php
 use yii\widgets\LinkPager;
-use yii\base\Object;
+// use yii\base\Object;
 use yii\bootstrap\ActiveForm;
 use common\utils\CommonFun;
 use yii\helpers\Url;
@@ -20,7 +20,7 @@ $modelLabel = new \backend\models\HubKolUser();
   <div class="row">
     <div class="col-xs-12">
       <div class="box">
-      
+
         <div class="box-header">
           <h3 class="box-title">数据列表</h3>
           <div class="box-tools">
@@ -32,14 +32,14 @@ $modelLabel = new \backend\models\HubKolUser();
           </div>
         </div>
         <!-- /.box-header -->
-        
+
         <div class="box-body">
           <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap table-responsive">
             <!-- row start search-->
           	<div class="row">
           	<div class="col-sm-12">
-                <?php ActiveForm::begin(['id' => 'wechat-user-search-form', 'method'=>'get', 'options' => ['class' => 'form-inline'], 'action'=>Url::toRoute('wechat-user/index')]); ?>     
-                
+                <?php ActiveForm::begin(['id' => 'wechat-user-search-form', 'method'=>'get', 'options' => ['class' => 'form-inline'], 'action'=>Url::toRoute('wechat-user/index')]); ?>
+
                   <div class="form-group" style="margin: 5px;">
                       <label><?=$modelLabel->getAttributeLabel('id')?>:</label>
                       <input type="text" class="form-control" id="query[id]" name="query[id]"  value="<?=isset($query["id"]) ? $query["id"] : "" ?>">
@@ -47,19 +47,19 @@ $modelLabel = new \backend\models\HubKolUser();
               <div class="form-group">
               	<a onclick="searchAction()" class="btn btn-primary btn-sm" href="#"> <i class="glyphicon glyphicon-zoom-in icon-white"></i>搜索</a>
            	  </div>
-               <?php ActiveForm::end(); ?> 
+               <?php ActiveForm::end(); ?>
             </div>
           	</div>
           	<!-- row end search -->
-          	
+
           	<!-- row start -->
           	<div class="row">
           	<div class="col-sm-12">
           	<table id="data_table" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="data_table_info">
             <thead class="text-nowrap">
             <tr role="row">
-            
-            <?php 
+
+            <?php
               $orderby = isset($_GET['orderby']) ? $_GET['orderby'] : '';
 		      echo '<th><input id="data_table_check" type="checkbox"></th>';
               echo '<th onclick="orderby(\'id\', \'desc\')" '.CommonFun::sortClass($orderby, 'id').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('id').'</th>';
@@ -78,14 +78,14 @@ $modelLabel = new \backend\models\HubKolUser();
               echo '<th onclick="orderby(\'pure_phone_number\', \'desc\')" '.CommonFun::sortClass($orderby, 'pure_phone_number').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('pure_phone_number').'</th>';
               echo '<th onclick="orderby(\'create_time\', \'desc\')" '.CommonFun::sortClass($orderby, 'create_time').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('create_time').'</th>';
               echo '<th onclick="orderby(\'update_time\', \'desc\')" '.CommonFun::sortClass($orderby, 'update_time').' tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >'.$modelLabel->getAttributeLabel('update_time').'</th>';
-         
+
 			?>
-	
+
             <th tabindex="0" aria-controls="data_table" rowspan="1" colspan="1" aria-sort="ascending" >操作</th>
             </tr>
             </thead>
             <tbody>
-            
+
             <?php
             foreach ($models as $model) {
                 echo '<tr id="rowid_' . $model->id . '">';
@@ -119,18 +119,18 @@ $modelLabel = new \backend\models\HubKolUser();
                 echo '  </td>';
                 echo '</tr>';
             }
-            
+
             ?>
-            
-           
-           
+
+
+
             </tbody>
             <!-- <tfoot></tfoot> -->
           </table>
           </div>
           </div>
           <!-- row end -->
-          
+
           <!-- row start -->
           <div class="row">
           	<div class="col-sm-5">
@@ -147,8 +147,8 @@ $modelLabel = new \backend\models\HubKolUser();
               	    'prevPageLabel' => '«',
               	    'firstPageLabel' => '首页',
               	    'lastPageLabel' => '尾页',
-              	]); ?>	
-              	
+              	]); ?>
+
               	</div>
           	</div>
 		  </div>
@@ -174,8 +174,8 @@ $modelLabel = new \backend\models\HubKolUser();
 				<h3>Settings</h3>
 			</div>
 			<div class="modal-body">
-                <?php $form = ActiveForm::begin(["id" => "wechat-user-form", "class"=>"form-horizontal", "action"=>Url::toRoute("wechat-user/save")]); ?>                      
-                 
+                <?php $form = ActiveForm::begin(["id" => "wechat-user-form", "class"=>"form-horizontal", "action"=>Url::toRoute("wechat-user/save")]); ?>
+
           <input type="hidden" class="form-control" id="id" name="id" />
 
           <div id="union_id_div" class="form-group">
@@ -297,9 +297,9 @@ $modelLabel = new \backend\models\HubKolUser();
               </div>
               <div class="clearfix"></div>
           </div>
-                    
 
-			<?php ActiveForm::end(); ?>          
+
+			<?php ActiveForm::end(); ?>
                 </div>
 			<div class="modal-footer">
 				<a href="#" class="btn btn-default" data-dismiss="modal">关闭</a> <a
@@ -315,11 +315,11 @@ function orderby(field, op){
 	 var url = window.location.search;
 	 var optemp = field + " desc";
 	 if(url.indexOf('orderby') != -1){
-		 url = url.replace(/orderby=([^&?]*)/ig,  function($0, $1){ 
+		 url = url.replace(/orderby=([^&?]*)/ig,  function($0, $1){
 			 var optemp = field + " desc";
 			 optemp = decodeURI($1) != optemp ? optemp : field + " asc";
 			 return "orderby=" + optemp;
-		   }); 
+		   });
 	 }
 	 else{
 		 if(url.indexOf('?') != -1){
@@ -329,7 +329,7 @@ function orderby(field, op){
 			 url = url + "?orderby=" + encodeURI(optemp);
 		 }
 	 }
-	 window.location.href=url; 
+	 window.location.href=url;
  }
  function searchAction(){
 		$('#wechat-user-search-form').submit();
@@ -356,7 +356,7 @@ function orderby(field, op){
         $("#pure_phone_number").val("");
         $("#create_time").val("");
         $("#update_time").val("");
-	
+
 	}
 	else{
     	        $("#id").val(data.id)
@@ -418,7 +418,7 @@ function orderby(field, op){
 }
 
 function initModel(id, type, fun){
-	
+
 	$.ajax({
 		   type: "GET",
 		   url: "<?=Url::toRoute('wechat-user/view')?>",
@@ -435,7 +435,7 @@ function initModel(id, type, fun){
    		   }
 		});
 }
-	
+
 function editAction(id){
 	initModel(id, 'edit');
 }
@@ -481,7 +481,7 @@ function deleteAction(id){
 	else{
 		admin_tool.alert('msg_info', '请先选择要删除的数据', 'warning');
 	}
-    
+
 }
 
 function getSelectedIdValues(formId)
@@ -525,7 +525,7 @@ $('#wechat-user-form').bind('submit', function(e) {
     	type: "post",
     	dataType:"json",
     	url: action,
-    	success: function(value) 
+    	success: function(value)
     	{
         	if(value.errno == 0){
         		$('#edit_dialog').modal('hide');
@@ -536,7 +536,7 @@ $('#wechat-user-form').bind('submit', function(e) {
             	var json = value.data;
         		for(var key in json){
         			$('#' + key).attr({'data-placement':'bottom', 'data-content':json[key], 'data-toggle':'popover'}).addClass('popover-show').popover('show');
-        			
+
         		}
         	}
 
